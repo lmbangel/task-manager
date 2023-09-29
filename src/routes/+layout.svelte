e707d54b2132cb298bb46fc17e6d5be7ba627a54
lmbangel <mbangelalihle@gmail.com>
2023-09-29 14:50:34 +0200

    init commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,324 @@
+<header class="mb-2">
+	<div id="kt_app_header" class="app-header">
+		<div
+			class="app-container container-fluid d-flex align-items-stretch justify-content-between"
+			id="kt_app_header_container"
+		>
+			<div class="d-flex align-items-center flex-grow-1 flex-lg-grow-0">
+				<a href="/" class="d-lg-none">
+					<img
+						alt="Logo"
+						src="https://theme.cdn-nucleus.live/assets/media/logos/default-small-dark.svg"
+						class="theme-dark-show h-30px"
+					/>
+				</a>
+			</div>
+			<div
+				class="d-flex align-items-stretch justify-content-between flex-lg-grow-1"
+				id="kt_app_header_wrapper"
+			>
+				<div
+					class="app-header-menu app-header-mobile-drawer align-items-stretch my-auto"
+					data-kt-drawer="true"
+					data-kt-drawer-name="app-header-menu"
+					data-kt-drawer-overlay="true"
+					data-kt-drawer-width="225px"
+					data-kt-drawer-direction="end"
+					data-kt-drawer-toggle="#kt_app_header_menu_toggle"
+					data-kt-swapper="true"
+				>
+					<span class="navbar-brand"><strong>KANSO</strong></span>
+					<div class="app-navbar-item my-auto px-8 brand-color">
+						<span class="mx-2" id=""> Task Manager </span>
+					</div>
+				</div>
+				<div class="app-navbar flex-shrink-0">
+					<div class="app-navbar-item align-items-stretch ms-1 ms-lg-3">
+						<div
+							id="kt_header_search"
+							class="header-search d-flex align-items-stretch"
+							data-kt-search-keypress="true"
+							data-kt-search-min-length="2"
+							data-kt-search-enter="enter"
+							data-kt-search-layout="menu"
+							data-kt-menu-trigger="auto"
+							data-kt-menu-overflow="false"
+							data-kt-menu-permanent="true"
+							data-kt-menu-placement="bottom-end"
+						>
+							<div
+								class="d-flex align-items-center"
+								data-kt-search-element="toggle"
+								id="kt_header_search_toggle"
+							>
+								<div
+									class="btn btn-icon btn-custom btn-icon-muted btn-active-light w-35px h-35px w-md-40px h-md-40px"
+								>
+									<span class="svg-icon svg-icon-1">
+										<svg
+											width="24"
+											height="24"
+											viewBox="0 0 24 24"
+											fill="none"
+											xmlns="http://www.w3.org/2000/svg"
+										>
+											<rect
+												opacity="0.5"
+												x="17.0365"
+												y="15.1223"
+												width="8.15546"
+												height="2"
+												rx="1"
+												transform="rotate(45 17.0365 15.1223)"
+												fill="currentColor"
+											/>
+											<path
+												d="M11 19C6.55556 19 3 15.4444 3 11C3 6.55556 6.55556 3 11 3C15.4444 3 19 6.55556 19 11C19 15.4444 15.4444 19 11 19ZM11 5C7.53333 5 5 7.53333 5 11C5 14.4667 7.53333 17 11 17C14.4667 17 17 14.4667 17 11C17 7.53333 14.4667 5 11 5Z"
+												fill="currentColor"
+											/>
+										</svg>
+									</span>
+								</div>
+							</div>
+							<div
+								data-kt-search-element="content"
+								class="menu menu-sub menu-sub-dropdown p-7 w-325px w-md-375px"
+							>
+								<div data-kt-search-element="wrapper">
+									<form
+										data-kt-search-element="form"
+										class="w-100 position-relative mb-3"
+										autocomplete="off"
+									>
+										<span
+											class="svg-icon svg-icon-2 svg-icon-lg-1 svg-icon-gray-500 position-absolute top-50 translate-middle-y ms-0"
+										>
+											<svg
+												width="24"
+												height="24"
+												viewBox="0 0 24 24"
+												fill="none"
+												xmlns="http://www.w3.org/2000/svg"
+											>
+												<rect
+													opacity="0.5"
+													x="17.0365"
+													y="15.1223"
+													width="8.15546"
+													height="2"
+													rx="1"
+													transform="rotate(45 17.0365 15.1223)"
+													fill="currentColor"
+												/>
+												<path
+													d="M11 19C6.55556 19 3 15.4444 3 11C3 6.55556 6.55556 3 11 3C15.4444 3 19 6.55556 19 11C19 15.4444 15.4444 19 11 19ZM11 5C7.53333 5 5 7.53333 5 11C5 14.4667 7.53333 17 11 17C14.4667 17 17 14.4667 17 11C17 7.53333 14.4667 5 11 5Z"
+													fill="currentColor"
+												/>
+											</svg>
+										</span>
+										<input
+											type="text"
+											class="search-input form-control form-control-flush ps-10"
+											name="search"
+											value=""
+											placeholder="Search..."
+											data-kt-search-element="input"
+										/>
+										<span
+											class="search-spinner position-absolute top-50 end-0 translate-middle-y lh-0 d-none me-1"
+											data-kt-search-element="spinner"
+										>
+											<span class="spinner-border h-15px w-15px align-middle text-gray-400" />
+										</span>
+										<span
+											class="search-reset btn btn-flush btn-active-color-primary position-absolute top-50 end-0 translate-middle-y lh-0 d-none"
+											data-kt-search-element="clear"
+										>
+											<span class="svg-icon svg-icon-2 svg-icon-lg-1 me-0">
+												<svg
+													width="24"
+													height="24"
+													viewBox="0 0 24 24"
+													fill="none"
+													xmlns="http://www.w3.org/2000/svg"
+												>
+													<rect
+														opacity="0.5"
+														x="6"
+														y="17.3137"
+														width="16"
+														height="2"
+														rx="1"
+														transform="rotate(-45 6 17.3137)"
+														fill="currentColor"
+													/>
+													<rect
+														x="7.41422"
+														y="6"
+														width="16"
+														height="2"
+														rx="1"
+														transform="rotate(45 7.41422 6)"
+														fill="currentColor"
+													/>
+												</svg>
+											</span>
+										</span>
+									</form>
+									<div class="separator border-gray-200 mb-6" />
+									<div data-kt-search-element="results" class="d-none" />
+									<div name="empty" class="text-center d-none">
+										<div class="pt-10 pb-10">
+											<span class="svg-icon svg-icon-4x opacity-50">
+												<svg
+													width="24"
+													height="24"
+													viewBox="0 0 24 24"
+													fill="none"
+													xmlns="http://www.w3.org/2000/svg"
+												>
+													<path
+														opacity="0.3"
+														d="M14 2H6C4.89543 2 4 2.89543 4 4V20C4 21.1046 4.89543 22 6 22H18C19.1046 22 20 21.1046 20 20V8L14 2Z"
+														fill="currentColor"
+													/>
+													<path
+														d="M20 8L14 2V6C14 7.10457 14.8954 8 16 8H20Z"
+														fill="currentColor"
+													/>
+													<rect
+														x="13.6993"
+														y="13.6656"
+														width="4.42828"
+														height="1.73089"
+														rx="0.865447"
+														transform="rotate(45 13.6993 13.6656)"
+														fill="currentColor"
+													/>
+													<path
+														d="M15 12C15 14.2 13.2 16 11 16C8.8 16 7 14.2 7 12C7 9.8 8.8 8 11 8C13.2 8 15 9.8 15 12ZM11 9.6C9.68 9.6 8.6 10.68 8.6 12C8.6 13.32 9.68 14.4 11 14.4C12.32 14.4 13.4 13.32 13.4 12C13.4 10.68 12.32 9.6 11 9.6Z"
+														fill="currentColor"
+													/>
+												</svg>
+											</span>
+										</div>
+										<div class="pb-15 fw-semibold">
+											<h3 class="text-gray-600 fs-5 mb-2">No result found</h3>
+											<div class="text-muted fs-7">Please try again with a different query</div>
+										</div>
+									</div>
+								</div>
+								<form data-kt-search-element="advanced-options-form" class="pt-1 d-none">
+									<h3 class="fw-semibold text-dark mb-7">Advanced Search</h3>
+								</form>
+							</div>
+						</div>
+					</div>
+
+					<div class="dropdown">
+						<ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+							<a
+								class="dropdown-item"
+								href="https://docs.google.com/presentation/d/1cDSfv73ABq7b7-1PQVaSS-EUYmQHEq7HS8wt355C9j8/edit#slide=id.g1281a005140_0_40"
+								target="_blank">Manual</a
+							>
+							<a class="dropdown-item" href="#" id="reportAnIssue">Report Issue</a>
+							<div class="dropdown-divider" />
+							<div class="dropdown-item">Learning Videos</div>
+							<a
+								class="dropdown-item"
+								data-fancybox
+								href="https://imagesnucleus.fra1.cdn.digitaloceanspaces.com/trainingvideos/nucleus/Nucleus%20Live%20Introduction.mp4"
+							>
+								Introduction (1:32)</a
+							>
+							<a
+								class="dropdown-item"
+								data-fancybox
+								href="https://imagesnucleus.fra1.cdn.digitaloceanspaces.com/trainingvideos/nucleus/How%20to%20access%20and%20share%20my%20eCard.mp4"
+							>
+								eCard - Use & Share (0:56)</a
+							>
+							<a
+								class="dropdown-item"
+								data-fancybox
+								href="https://imagesnucleus.fra1.cdn.digitaloceanspaces.com/trainingvideos/nucleus/Where%20to%20find%20assistance%20and%20log%20a%20Support%20Ticket.mp4"
+							>
+								Support - Finding & Tickets (0:58)</a
+							>
+						</ul>
+					</div>
+
+					<div class="app-navbar-item" id="kt_header_user_menu_toggle">
+						<a href="/logout" class="btn my-4 mx-1" type="button"> Sign Out </a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</header>
+
+<slot />
+
+<!-- <footer>Footer</footer> -->
+
+<style>
+	.app-header {
+		background-color: #ffff !important;
+	}
+
+	.branches-list-select {
+		min-width: 20rem;
+	}
+
+	#kt_header_user_menu_toggle > .btn {
+		color: #093c71;
+		font-weight: 500;
+	}
+
+	#kt_header_user_menu_toggle > .btn:hover {
+		text-decoration: underline !important;
+		background-color: rgba(9, 43, 113, 0.1);
+	}
+
+	#dropdownMenuButton1 {
+		color: #093c71;
+		border-color: #093c71 !important;
+	}
+
+	#dropdownMenuButton1:hover {
+		border-color: #093c71;
+		text-decoration: underline !important;
+		background-color: rgba(9, 43, 113, 0.1);
+	}
+
+	.dropdown-menu > .dropdown-item:hover {
+		border-color: #093c71;
+		text-decoration: underline !important;
+		background-color: rgba(9, 43, 113, 0.1);
+	}
+
+	.svg-icon-1:hover {
+		color: #093c71 !important;
+	}
+
+	.search-item {
+		color: #093c71;
+	}
+
+	.search-item:hover {
+		text-decoration: underline !important;
+	}
+
+	.select2.select2-container {
+		min-width: 18rem;
+	}
+	.navbar-brand {
+		color: #999b9e;
+		font-size: 24px;
+		font-weight: 700;
+	}
+	.brand-color {
+		color: #999b9e;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,46 @@
+<div class="card card-flush">
+	<div class="card-header pt-8">
+		<div class="mr-auto" id="new-task">
+			<a href="" class="btn border border-0 py-2 px-10" type="button"> New Task</a>
+		</div>
+	</div>
+	<div class="card-body">
+		<div class="row">
+			<div
+				id="sales-data"
+				style="overflow-x:scroll;  border-collapse: collapse;border-spacing: 0;width: 100%;"
+			>
+				<table class="table table-hover responsive no-wrap" id="sales-data-table">
+					<thead class="border-bottom">
+						<tr>
+							<th>Task</th>
+							<th>Description</th>
+							<th>Owner</th>
+							<th>Status</th>
+							<th>Priority</th>
+							<th>Type</th>
+							<th>Role</th>
+							<th>Due Date</th>
+							<th />
+						</tr>
+					</thead>
+					<tbody />
+				</table>
+			</div>
+		</div>
+	</div>
+</div>
+
+<style>
+	#new-task > .btn {
+		color: #093c71;
+		font-weight: 500;
+		background-color: rgba(9, 43, 113, 0.1);
+	}
+
+	#new-task > .btn:hover {
+		text-decoration: underline !important;
+		color: #fff;
+		background-color: rgba(9, 43, 113, 1);
+	}
+</style>
